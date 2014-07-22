be7245c3535e54a73b719690dcecc6c017e6141d
indotek68 <angga.w.putra@gmail.com>
2014-07-22 16:16:25 -0700

    OMDB works

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser")
+var path = require('path');
 
 var app = express();
+app.use(express.static(__dirname + '/assets'));
+
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.render('index.ejs');
+  res.render('index');
+});
+
+app.get('/search', function(req, res){
+  var query = req.query.searchTerm;
+  var url = "http://www.omdbapi.com/?s=" + query;
+  
+
+  request(url, function(error, response, body){
+  	if(!error){
+  		var body = JSON.parse(body);
+  		res.render('results', {movieList: body.Search});
+  	}
+  });
 });
 
 app.listen(3000);
